d5fb2bbaa918e3696ab5f713bf5e8cb8107c89c5
alexander <a.samoyl11@gmail.com>
2019-11-21 18:16:56 +0300

    yandex and google metrics

--- src/components/Services.js
+++ src/components/Services.js
@@ -1,8 +1,8 @@
 import React from 'react';
-import {Container, Row, Col} from 'react-bootstrap';
+import {Container, Row, Col, Figure} from 'react-bootstrap';
 import {Spring} from 'react-spring/renderprops';
-// import LocalTaxiIcon from '@material-ui/icons/LocalTaxi';
-// import LocalShippingIcon from '@material-ui/icons/LocalShipping';
+import LocalTaxiIcon from '@material-ui/icons/LocalTaxi';
+import LocalShippingIcon from '@material-ui/icons/LocalShipping';
 import CheckCircleOutlineIcon from '@material-ui/icons/CheckCircleOutline';
 
 import {
@@ -15,17 +15,31 @@ class Services extends React.Component {
     this.service = this.service.bind(this);
   }
 
-  service(r) {
-    return (
-      <Container className='my-4 px-2' style={{background: '#FEFE25', 'border-radius': '2em'}}>
-        <Link to='/apply' style={{ textDecoration: 'none'}}>
-          <Row>
-            <Col xs={0.5}><CheckCircleOutlineIcon className="my-3 ml-4" style={{'font-size': '70px', color:'black'}}/></Col>
-            <Col className='mx-1 my-auto font-question' style={{color:'black'}}>{r}</Col>
-          </Row>
-        </Link>
-      </Container>
-    )
+  service(r, taxi=true) {
+    if (taxi === true) {
+      return (
+        <Container className='my-4 px-2' style={{background: '#FEFE25', 'border-radius': '2em'}}>
+          <Link to='/apply' style={{ textDecoration: 'none' }}>
+            <Row>
+              <Col xs={0.5}><CheckCircleOutlineIcon className="my-3 ml-4" style={{'font-size': '70px', color:'black'}}/></Col>
+              <Col className='mx-1 my-auto font-question' style={{color:'black'}}>{r}</Col>
+            </Row>
+          </Link>
+        </Container>
+      )
+    }
+    else {
+      return (
+        <Container className='my-4 px-2' style={{background: '#FEFE25', 'border-radius': '2em'}}>
+          <Link to='/apply' style={{ textDecoration: 'none' }}>
+            <Row>
+              <Col xs={0.5}><CheckCircleOutlineIcon className="my-3 ml-4" style={{'font-size': '70px', color: 'black'}}/></Col>
+              <Col className='mx-1 my-auto font-question' style={{color:'black'}}>{r}</Col>
+            </Row>
+          </Link>
+        </Container>
+      )
+    }
   }
 
   render() {
@@ -42,13 +56,13 @@ class Services extends React.Component {
           </Spring>
         </div>
         {this.service("Яндекс.Такси")}
-        {this.service("Ситимобил")}
+        {this.service("СитиМобил")}
         {this.service("Gett")}
         {this.service("Uber")}
         {this.service("Трезвый водитель")}
-        {this.service("Доставка")}
-        {this.service("Доставка грузов")}
-        {this.service("Грузовое такси")}
+        {this.service("Доставка", false)}
+        {this.service("Доставка грузов", false)}
+        {this.service("Грузовое такси", false)}
 
 
       </div>
